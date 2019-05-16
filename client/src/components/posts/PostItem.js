0386b5803a723fe52f8823ce83f6690559477506
Daniel Duany <dduany919@gmail.com>
2019-05-15 21:33:24 -0400

    Add and delete posts, add comments and single post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -10,50 +10,64 @@ const PostItem = ({
   removeLike,
   deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => (
   <div class='post bg-white p-1 my-1'>
     <div>
-      <a href='profile.html'>
+      <Link to={`/profile/${user}`}>
         <img class='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
     <div>
       <p class='my-1'>{text}</p>
       <p class='post-date'>
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button onClick={e => addLike(_id)} type='button' class='btn btn-light'>
-        <i class='fas fa-thumbs-up' />{' '}
-        <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-      </button>
-      <button
-        onClick={e => removeLike(_id)}
-        type='button'
-        class='btn btn-light'
-      >
-        <i class='fas fa-thumbs-down' />
-      </button>
-      <Link to={`/post/${_id}`} class='btn btn-primary'>
-        Discussion{' '}
-        {comments.length > 0 && (
-          <span class='comment-count'>{comments.length}</span>
-        )}
-      </Link>
-      {!auth.loading &&
-      user === auth.user._id && ( //Even thought this is taking away the button from the user, it is also protected on the backout so only the designated user can delete their own posts
+
+      {showActions && (
+        <Fragment>
           <button
-            onClick={e => deletePost(_id)}
+            onClick={e => addLike(_id)}
             type='button'
-            class='btn btn-danger'
+            class='btn btn-light'
           >
-            <i class='fas fa-times' />
+            <i class='fas fa-thumbs-up' />{' '}
+            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
-        )}
+          <button
+            onClick={e => removeLike(_id)}
+            type='button'
+            class='btn btn-light'
+          >
+            <i class='fas fa-thumbs-down' />
+          </button>
+          <Link to={`/posts/${_id}`} class='btn btn-primary'>
+            Discussion{' '}
+            {comments.length > 0 && (
+              <span class='comment-count'>{comments.length}</span>
+            )}
+          </Link>
+          {!auth.loading &&
+          user === auth.user._id && ( //Even thought this is taking away the button from the user, it is also protected on the backout so only the designated user can delete their own posts
+              <button
+                onClick={e => deletePost(_id)}
+                type='button'
+                class='btn btn-danger'
+              >
+                <i class='fas fa-times' />
+              </button>
+            )}
+        </Fragment>
+      )}
     </div>
   </div>
 );
+
+PostItem.defaultProps = {
+  showActions: true
+};
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
